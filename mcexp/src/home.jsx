0da721ff417fea387adc4c6c24ad6aa981116268
kavin.npc <developerkavinxx@gmail.com>
2024-09-09 22:19:20 +0530

    update

--- mcexp/src/home.jsx
+++ mcexp/src/home.jsx
@@ -4,13 +4,13 @@ import AboutUsModal from './projects/about-us';
 const Home = () => {
   return (
     <div className="button-container">
-      <AboutUsModal
-         buttonText="About-Us"
-         title="About Us"
-       >
-         <p>Bla bla bla...</p>
+      <AboutUsModal buttonText="About Us" title="About PostPilot">
+        <p>Welcome to PostPilot! We're a dynamic duo passionate about simplifying social media management. Our mission is to help you seamlessly schedule posts across Instagram, Facebook, Snapchat, and more.</p>
+        <p>At PostPilot, we understand the demands of managing multiple accounts. That's why we've built a platform to <span className="text-teal-400">streamline your workflow</span>, save you time, and boost your online presence.</p>
+        <p>Join us as we make social media scheduling <span className="text-coral-400">easier and more efficient</span>. Together, we'll take your online presence to new heights!</p>
       </AboutUsModal>
     </div>
+
   );
 }
 
